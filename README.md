794e7274ec27a3dcc7251c3b7b29f9aae344ec9c
Shyam Chandran <shyamchandranmec@gmail.com>
2016-07-23 18:56:46 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Blogger
+install node js
+node version >= 4.0.0
+
+npm install
+
+node bin/www
+
+localhost:3000/
---
+++ public/javascripts/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by shyam on 23/07/16.
+ */
+$(document).ready(function () {
+    $('.button-collapse').sideNav({
+            menuWidth: 240,
+            edge: 'right',
+            closeOnClick: true
+        }
+    );
+})
